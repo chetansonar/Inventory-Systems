f5b0fdf96cd23f7397544925a6550f8b7a37cdd0
chetansonar <chetansonar.17@gmail.com>
2018-10-23 14:49:43 +0530

    Modified

--- Invent_23-10-2016.sql
+++ Invent_23-10-2016.sql
@@ -15,7 +15,7 @@ truncate Inventory.annual_spend;
 insert into Inventory.annual_spend select *,(unit_cost * annual_unit_demand) from Inventory.annual_data_usage ;
 
 truncate Inventory.annual_spend1;
-insert into Inventory.annual_spend1  select * from Inventory.annual_spend order by annual_spend desc;
+insert into Inventory.annual_spend1 select * from Inventory.annual_spend order by annual_spend desc;
 
 truncate Inventory.Cumulative_Running_Total;
 insert into Inventory.Cumulative_Running_Total select  *,
@@ -24,9 +24,9 @@ from Inventory.annual_spend1 t1 ;
 
 truncate Inventory.Class_A_Items;
 insert into Inventory.Class_A_Items select *,(select case when 
-	Cumulative_annual_spend <  ( select CAST(sum(annual_spend)*(80/100) as char) from Inventory.Cumulative_Running_Total) then 'A' 
+	Cumulative_annual_spend <  (select CAST(sum(annual_spend)*(80/100) as char) from Inventory.Cumulative_Running_Total) then 'A' 
 when Cumulative_annual_spend < (select CAST(sum(annual_spend)*(95/100) as char) from Inventory.Cumulative_Running_Total) then 'B'
-when Cumulative_annual_spend <= (select CAST(sum(annual_spend) as char) from Inventory.Cumulative_Running_Total) then 'C'
+else 'C'
 end )from Inventory.Cumulative_Running_Total ;
 
 
